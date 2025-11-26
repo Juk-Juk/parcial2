70183a1f838d6b4e869735b8d30dd6cbe9cfd6d4
Juk <Jukzhee@gmail.com>
2025-11-26 16:49:35 -0300

    deploy11

--- cuentas/views.py
+++ cuentas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.core.mail import send_mail
+from django.conf import settings
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import RegistroForm
 
@@ -7,10 +9,23 @@ def registro(request):
     if request.method == 'POST':
         form = RegistroForm(request.POST)
         if form.is_valid():
-            form.save()
+            # Save the form to create the user
+            user = form.save()
+
+            # Send a welcome email
+            subject = '¡Bienvenido a nuestro sitio web!'
+            message = f'Hola {user.username},\n\nGracias por registrarte en nuestro sitio web.\n\n¡Disfruta de tu experiencia!'
+            from_email = settings.EMAIL_HOST_USER  # Use the email from settings
+            recipient_list = [user.email]  # Send to the new user's email
+
+            # Send the email
+            send_mail(subject, message, from_email, recipient_list)
+
+            # Redirect the user to the login page after registration
             return redirect('cuentas:login')
     else:
         form = RegistroForm()
+    
     return render(request, 'registro.html', {'form': form})
 
 def login_view(request):
