5eeab7837df982c4b8a81799d4dab6955116efad
Juk <Jukzhee@gmail.com>
2025-11-26 15:14:57 -0300

    deploy5

--- parcial_alumnos/settings.py
+++ parcial_alumnos/settings.py
@@ -133,13 +133,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 if DEBUG==False:
 	STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 	STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-else:
-    STATIC_URL = 'static/'
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
