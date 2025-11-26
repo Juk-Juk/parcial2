660e5de5e77a399f5135feea9d93a3c006d15a4a
Juk <Jukzhee@gmail.com>
2025-11-26 15:04:44 -0300

    deploy3

--- parcial_alumnos/settings.py
+++ parcial_alumnos/settings.py
@@ -90,17 +90,13 @@ WSGI_APPLICATION = 'parcial_alumnos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if DEBUG==False:
-   	DATABASES = {
-        	"default": dj_database_url.config(default=env("DATABASE_URL"), conn_max_age=600)
-    	}
-else:
-   	DATABASES = {
-        	'default': {
-            	'ENGINE': 'django.db.backends.sqlite3',
-            	'NAME': BASE_DIR / 'db.sqlite3',
-        	}
-    	}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
